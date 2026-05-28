9f48adcaf232c8c3f9d5ed0ffec79557052695b4
wangjingan100 <49703177+wangjingan100@users.noreply.github.com>
2026-05-28 17:49:20 +0800

    添加订单控制器

---
+++ WebApiDockerCICD/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDockerCICD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+
+        /// <summary>
+        /// Get Order Details
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public string GetOrder()
+        {
+            return "Hello World Get Order Details 获取订单详情";
+        }
+    }
+}
