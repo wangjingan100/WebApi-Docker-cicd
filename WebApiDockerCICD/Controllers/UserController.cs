791565c5e4d6e1557b85b995670cebcc6ab3b8bf
wangjingan100 <49703177+wangjingan100@users.noreply.github.com>
2026-05-28 16:57:41 +0800

    1 开放生产环境 swagger 2 添加用户控制器

---
+++ WebApiDockerCICD/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDockerCICD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        /// <summary>
+        /// Get User Name
+        /// </summary>
+        /// <returns></returns>
+        public string GetUser()
+        {
+            return "Hello World Get UserName";
+        }
+    }
+}
