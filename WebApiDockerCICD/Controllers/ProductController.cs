96ada75ffe89b6b617529448670f77265a653d05
wangjingan100 <49703177+wangjingan100@users.noreply.github.com>
2026-05-31 15:26:09 +0800

    5675

---
+++ WebApiDockerCICD/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDockerCICD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { Message = "Hello from ProductController!" });
+        }
+    }
+}
